77b08dccac30d62319f0d1be507309b1779c38cb
hendrickm <hendrick.lima@gmail.com>
2020-02-09 12:56:39 -0300

    Ajusta váriavel undefined

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const request = require('request');
 
 const app = express();
 
-const API_KEY = 'RGAPI-61c00181-a8f6-485b-b31f-d696dfbb0728'
+const API_KEY = 'RGAPI-1f4ffcd7-f589-467a-ac56-727ed9b7011d'
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
@@ -12,13 +12,11 @@ app.use((req, res, next) => {
 
 app.get('/champion-mastery/by-summoner', (req, res) => {
   const {id, region} = req.query;
-  console.log(id)
-  console.log(region)
   request(
     { url: `https://${region}.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/${id}?api_key=${API_KEY}` },
     (error, response, body) => {
       if (error || response.statusCode !== 200) {
-        return res.status(500).json({ type: 'error', message: err });
+        return res.status(500).json({ type: 'error', message: error });
       }
       res.json(JSON.parse(body));
     }
@@ -31,7 +29,7 @@ app.get('/summoners/by-name', (req, res) => {
     { url: `https://${region}.api.riotgames.com/lol/summoner/v4/summoners/by-name/${summonerName}?api_key=${API_KEY}` },
     (error, response, body) => {
       if (error || response.statusCode !== 200) {
-        return res.status(500).json({ type: 'error', message: err.message });
+        return res.status(500).json({ type: 'error', message: error });
       }
       res.json(JSON.parse(body));
     }

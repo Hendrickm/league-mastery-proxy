12ca11717854a7bee440e3011ec67ddacb94795c
hendrickm <hendrick.lima@gmail.com>
2020-02-10 21:11:43 -0300

    Adiciona endpoint para carregar partidas

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const request = require('request');
 
 const app = express();
 
-const API_KEY = 'RGAPI-1f4ffcd7-f589-467a-ac56-727ed9b7011d'
+const API_KEY = 'RGAPI-a1bb881a-3b7f-4a31-8268-96afa2596d8d'
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
@@ -27,6 +27,21 @@ app.get('/summoners/by-name', (req, res) => {
   const {summonerName, region} = req.query;
   request(
     { url: `https://${region}.api.riotgames.com/lol/summoner/v4/summoners/by-name/${summonerName}?api_key=${API_KEY}` },
+    (error, response, body) => {
+      if (error || response.statusCode !== 200) {
+        return res.status(500).json({ type: 'error', message: error });
+      }
+      res.json(JSON.parse(body));
+    }
+  )
+});
+
+app.get('/matchlists/by-account', (req, res) => {
+  const {accountId, region} = req.query;
+  const queryParams = { beginIndex: '0', endIndex:'20' };
+
+  request(
+    { url: `https://${region}.api.riotgames.com/lol/match/v4/matchlists/by-account/${accountId}?api_key=${API_KEY}`,qs: queryParams },
     (error, response, body) => {
       if (error || response.statusCode !== 200) {
         return res.status(500).json({ type: 'error', message: error });
